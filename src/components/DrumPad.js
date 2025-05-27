f5d31fb801a8c394e856a250c6207f2600307025
Syan <torressyanvale@gmail.com>
2025-05-28 07:18:29 +0800

    added effect onclick

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,14 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 const DrumPad = ({ keyTrigger, clipId, url, updateDisplay, volume}) => {
+  const [isActive, setIsActive] = useState(false);
   //function to play the sound and update the display.
   const playSound = () => {
     const audio = new Audio(url); // Create a new Audio object
     audio.volume = volume;        // Apply volume control
-    audio.currentTime = 0;        // Start from the beginning
+    audio.currentTime = 0.1;        // Start from the beginning
     audio.play();                 // Play sound immediately
     updateDisplay(clipId);        // Update the UI
+
+    // Trigger active class
+    setIsActive(true);
+    setTimeout(() => setIsActive(false), 150); // Remove class after animation
   }
 
   // Listen for keyboard events to trigger the correct pad
